c779bb35d0d4bad16b066d80c38b5c2dc54ea7a1
abhishekwl <nateriver210@gmail.com>
2018-09-10 01:24:03 +0530

    Updated port number in config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 8000 || process.env.PORT,
+    PORT: 8081 || process.env.PORT,
     BASE_URL: '/api/v1/',
     MONGODB_URI: 'mongodb+srv://abhishekwl:abcd1234@primarycluster-hunt-3fr7d.mongodb.net/test?retryWrites=false'
 };
